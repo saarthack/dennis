6f9b504611b58ce321b2052193fc9cbfe8c45db7
Sarthak Sharma <sarthaksharma@Sarthaks-Laptop.local>
2024-06-10 18:22:46 +0530

    made page1 text animation

--- src/components/LandingPage/HeroText.jsx
+++ src/components/LandingPage/HeroText.jsx
@@ -1,15 +1,55 @@
-import React from 'react';
+import { motion, useScroll } from 'framer-motion';
+import React, { useEffect, useState } from 'react';
 
 const HeroText = () => {
 
+  const { scrollY } = useScroll()
+
+  const [direction, setDirection] = useState(1)
+
+  // agar sidha scroll kar rhe hai to direction ki value 1 and  
+  // agar ulta scroll kar rhe hai to direction ki value -1
+
+  useEffect(() => {
+
+    const handleScroll = () => {
+      const scrollingValue = scrollY.get() - scrollY.getPrevious()
+
+      if (scrollingValue > 0) {
+        setDirection(1)
+      } else {
+        setDirection(-1)
+      }
+    }
+
+    scrollY.on("change", handleScroll)
+
+  }, [scrollY]);
+
+
+
   return (
-    <div className='absolute top-[60%] whitespace-nowrap'>
+    <motion.div
+      className='absolute top-[60%] whitespace-nowrap'
+
+      initial={{
+        x: -3350
+      }}
+      animate={{
+        x: direction > 0 ? -6700 : 0
+      }}
+      transition={{
+        duration: 8,
+        repeat: Infinity,
+        ease: "linear"
+      }}
+    >
       <h1 className='whitespace-nowrap inline-block font-normal text-[15vw] text-white'>Dennis Snellenberg - </h1>
       <h1 className='whitespace-nowrap inline-block font-normal text-[15vw] text-white'>Dennis Snellenberg - </h1>
       <h1 className='whitespace-nowrap inline-block font-normal text-[15vw] text-white'>Dennis Snellenberg - </h1>
       <h1 className='whitespace-nowrap inline-block font-normal text-[15vw] text-white'>Dennis Snellenberg - </h1>
       <h1 className='whitespace-nowrap inline-block font-normal text-[15vw] text-white'>Dennis Snellenberg - </h1>
-    </div>
+    </motion.div>
   );
 };
 
